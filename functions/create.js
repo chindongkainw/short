94b01be8ba50c0e841233d8d5098e992cc734c1a
钦东凯文 <78853981+chindongkainw@users.noreply.github.com>
2024-09-07 00:30:10 +0800

    Update create.js

--- functions/create.js
+++ functions/create.js
@@ -5,7 +5,7 @@
 // Path: functions/create.js
 
 function generateRandomString(length) {
-    const characters = '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const characters = '1234567890abcdefghijkmnpqrstuvwxyz';
     let result = '';
 
     for (let i = 0; i < length; i++) {
